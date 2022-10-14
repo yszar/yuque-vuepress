608bc7b42ec2e8a1b0d9886475cd03b0e08097c3
yszar <yszar@vip.qq.com>
2022-10-15 04:34:15 +0800

    fix: :bug: 修复bug

--- commitlint.config.js
+++ commitlint.config.js
@@ -23,39 +23,39 @@ module.exports = {
             confirmCommit: '是否提交或修改commit ?'
         },
         types: [
-            {value: 'feat', name: 'feat:     ✨ 新增功能 | A new feature', emoji: "✨"},
-            {value: 'fix', name: 'fix:      🐛 修复缺陷 | A bug fix', emoji: ":bug:"},
-            {value: 'docs', name: 'docs:     📝 文档更新 | Documentation only changes', emoji: "🐛"},
+            {value: 'feat', name: 'feat:     新增功能 | A new feature', emoji: ":sparkles:"},
+            {value: 'fix',  name: 'fix:      修复缺陷 | A bug fix', emoji: ":bug:"},
+            {value: 'docs', name: 'docs:     文档更新 | Documentation only changes', emoji: ":memo:"},
             {
                 value: 'style',
-                name: 'style:    💄 代码格式 | Changes that do not affect the meaning of the code',
+                name: 'style:    代码格式 | Changes that do not affect the meaning of the code',
                 emoji: ":lipstick:"
             },
             {
                 value: 'refactor',
-                name: 'refactor:  ♻️代码重构 | A code change that neither fixes a bug nor adds a feature',
+                name: 'refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature',
                 emoji: ":recycle:"
             },
-            {value: 'perf', name: 'perf:     ⚡️ 性能提升 | A code change that improves performance', emoji: ":zap:"},
+            {value: 'perf', name: 'perf:     性能提升 | A code change that improves performance', emoji: ":zap:"},
             {
                 value: 'test',
-                name: 'test:     ✅ 测试相关 | Adding missing tests or correcting existing tests',
+                name: 'test:     测试相关 | Adding missing tests or correcting existing tests',
                 emoji: ":white_check_mark:"
             },
             {
                 value: 'build',
-                name: 'build:    📦️ 构建相关 | Changes that affect the build system or external dependencies',
+                name: 'build:    构建相关 | Changes that affect the build system or external dependencies',
                 emoji: ":package:"
             },
             {
                 value: 'ci',
-                name: 'ci:       🎡 持续集成 | Changes to our CI configuration files and scripts',
+                name: 'ci:       持续集成 | Changes to our CI configuration files and scripts',
                 emoji: ":ferris_wheel:"
             },
-            {value: 'revert', name: 'revert:   ⏪️ 回退代码 | Revert to a commit', emoji: ":hammer:"},
+            {value: 'revert', name: 'revert:   回退代码 | Revert to a commit', emoji: ":hammer:"},
             {
                 value: 'chore',
-                name: 'chore:    🔨 其他修改 | Other changes that do not modify src or test files',
+                name: 'chore:    其他修改 | Other changes that do not modify src or test files',
                 emoji: ":rewind:"
             },
         ],
